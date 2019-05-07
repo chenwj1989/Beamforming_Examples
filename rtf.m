c5872943cc131a0b26c0396896fa66a0d16ee2a7
chen weijian <wjchen@astri.org>
2019-05-07 23:52:17 +0800

    Calculate RTF for multiple frequencies

--- rtf.m
+++ rtf.m
@@ -2,16 +2,22 @@
 %--------------------
 % Input parameters
 %--------------------
-%Ry :   The PSD matrix of input signal,  [numMics x num_mics x num_freq ]
-%Rn :   The PSD matrix of estimated noise,  [numMics x num_mics x num_freq ]
+%Ry :   The PSD matrix of input signal,  [numMics x numMics x numFreqs ]
+%Rn :   The PSD matrix of estimated noise,  [numMics x numMics x numFreqs ]
 %--------------------
 % Output parameters
 %--------------------
-%h: The estimated reative transfer function [ numMics x num_freq ] 
+%h: The estimated reative transfer function [ numMics x numFreqs ] 
 function [h] = rtf(Ry, Rn)
-    N =  size(Ry, 1);
+    numMics =  size(Ry, 1);
+    numFreqs = size(Ry, 3);
     Rx = Ry - Rn;
-    e =  [1; zeros(N-1, 1)];
-    h = (Rx * e) ./ (e.' * Rx * e);
+    
+    e =  [1; zeros(numMics-1, 1)];
+    h = zeros(numMics, numFreqs);
+    for i = 1:numFreqs
+        h(:, i) = (Rx(:, :, i) * e) ./ (e.' * Rx(:, :, i) * e);
+    end
+    
 
 
